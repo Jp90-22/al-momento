521c5c6560b9b22107cd236b352e3abe5bdcdf3e
José Otniel Paulino Frías <joseotnielp@outlook.es>
2024-07-06 00:06:36 -0400

    Add "Noticias destacadas" and "Ultimas noticias agregadas" section, and fix some design problems.

--- AlMomento/Pages/Index.cshtml.cs
+++ AlMomento/Pages/Index.cshtml.cs
@@ -1,20 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AlMomento.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace AlMomento.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly AlMomento.Data.AlMomentoContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(AlMomento.Data.AlMomentoContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
-        {
+        public IList<Noticia> NoticiasDestacadas { get; set; } = default!;
+        public IList<Noticia> NoticiasRecientes { get; set; } = default!;
 
+        public async Task OnGetAsync()
+        {
+            if (_context.Noticia != null)
+            {
+                NoticiasDestacadas = await _context.Noticia.OrderByDescending(noticia => noticia.CantidadVisitas)
+                    .Take(6).ToListAsync();
+
+                NoticiasRecientes = await _context.Noticia.OrderByDescending(noticia => noticia.FechaPublicacion)
+                    .Take(6).ToListAsync();
+            }
         }
     }
 }
